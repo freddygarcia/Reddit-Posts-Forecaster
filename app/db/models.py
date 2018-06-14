2ce3e84906e3d5002a41f730570836360f11612a
Freddy <buscando_alink@hotmail.com>
2018-06-14 12:14:46 -0400

    Remove unused field 'content'

--- app/db/models.py
+++ app/db/models.py
@@ -11,12 +11,10 @@ class Post(Base):
 
     id = Column(Integer, primary_key=True)
     created_at = Column(TIMESTAMP, nullable=False)
-    content = Column(String(220), nullable=False)
     permalink = Column(String(120), nullable=False)
 
     def to_dict(self):
         return {
             'created_at': self.created_at,
-            'content': self.content,
             'permalink': self.permalink
         }
