d9ab908d5f224cd1e117e125d69e5b3ea2ccdb52
Freddy <buscando_alink@hotmail.com>
2018-06-16 17:57:21 -0400

    Prepare main with the basic 3 functions

--- main.py
+++ main.py
@@ -1,86 +1,64 @@
-from math import ceil
-from datetime import datetime, timedelta
-
 from pandas import DataFrame
-from matplotlib import style
-
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.linear_model import LinearRegression
-
-import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
 
 from app.db.connection import session
 from app.db.models import Post
 
 
-def get_posts(ts_from, ts_to):
-    '''Retrieve all posts by given time filter'''
+def get_posts(ts_from=None, ts_to=None):
+    '''Retrieve all posts by given time filter.
 
-    posts = (session.query(Post)
-             .filter(Post.created_at
-                     .between(ts_from, ts_to))).all()
+    I just want ts_from to be optional, but
+    if parameters were inverted, may be confusing.
+    '''
+
+    if not (ts_from or ts_to): return None
+
+    query = session.query(Post)
+    posts = None
+
+    if ts_from:
+        posts = (query.filter(Post.created_at
+                .between(ts_from, ts_to))).all()
+    else:
+        posts = query.filter(Post.created_at < ts_to).all()
 
     if posts:
         posts_df = DataFrame(list(map(lambda post: post.to_dict(), posts)))
         posts_df.set_index('created_at', inplace=True)
 
-        # group every 30 minuts
+        # group every 30 minutes
         MINUTES_30 = '30T'
         return posts_df.resample(MINUTES_30).count()
 
+    return DataFrame()
 
-def plots(posts_df, graph_type='bar'):
-    _df.plot()
+
+def plots(posts_df):
+    posts_df.plot()
     plt.show()
 
 
-def predict(time=None):
-    DAYS = 20
+def predict(ts):
+    '''Give the aprox number of posts between `ts` and `ts + 1 hour`
+    using data that has `created_at < ts`.
 
-    a =  datetime(2018,6,1,2,55)
-    b = a + timedelta(days=DAYS)
+    All the details is in app.prediction module
 
-    posts = get_posts(a, b)
+    Parameters
+    ----------
+    ts : datetime
+        date to predict the number of posts
 
-    posts['day'] = posts['time'].apply(lambda x: x.day).astype(float)
-    posts['hour'] = posts['time'].apply(lambda x: x.hour).astype(float)
-    posts['minute'] = posts['time'].apply(lambda x: x.minute).astype(float)
+    Returns
+    -------
+    int
+        the number of posts between ts and ts + 1 hour
+    '''
+    from app.prediction import Forecaster
 
-    PERCENT_TO_PREDICT = 40
+    train, test = Forecaster.train_test_data(ts)
+    forecaster = Forecaster.forecaster(train)
 
-    c = posts['count'].values.reshape((len(posts['count']), 1))
+    return forecaster.predict(ts)
 
-    forecast_col = 'count'
-    forecast_out = int(ceil( (PERCENT_TO_PREDICT / 100) * len(posts)))
-
-    posts['label'] = posts[forecast_col].shift(-forecast_out)
-
-    clean_data = posts.dropna()
-    clean_data.set_index('time', inplace=True)
-
-
-    X = np.array(clean_data.drop(['label', 'count'], 1))
-
-    scaler = MinMaxScaler()
-    scaler.fit(X)
-    X = scaler.transform(X)
-
-    X_lately= X[-forecast_out:]
-
-    clean_data = clean_data.dropna()
-    y = np.array(clean_data['label'])
-
-    x_t, x_test, y_t, y_test = train_test_split(X, y, shuffle=False, test_size=.2)
-
-    ln = LinearRegression()
-    ln.fit(x_t, y_t)
-    accuracy = ln.score(x_test, y_test)
-
-    return ln.predict(time)
-    # accuracy = ln.score(x_test, y_test)
-    # forecast_set = ln.predict(X_lately)
-
-    # return accuracy
