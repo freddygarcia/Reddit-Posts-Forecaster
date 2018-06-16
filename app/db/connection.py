bd360d7f2e6a148c4172634ccf6e38a98ccc76f7
Freddy <buscando_alink@hotmail.com>
2018-06-16 17:19:28 -0400

    Change connection engine to sqlite

--- app/db/connection.py
+++ app/db/connection.py
@@ -2,19 +2,14 @@ from app.db.models import Base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DB_CONFIG = {
-    'USER': 'root',
-    'HOST': 'localhost',
-    'DB': 'bar',
-}
 
+engine = create_engine('sqlite:///test.db')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
 
-
-# ENGINE = create_engine('sqlite:///test.db')
-# Session = sessionmaker(bind=ENGINE)
+# DB_CONFIG = {'USER': 'root', 'HOST': 'localhost', 'DB': 'bar' }
+# engine = create_engine('mysql://{USER}@{HOST}/{DB}'.format(**DB_CONFIG))
+# Base.metadata.create_all(engine)
+# Session = sessionmaker(bind=engine)
 # session = Session()
-
-ENGINE_mysl = create_engine('mysql://{USER}@{HOST}/{DB}'.format(**DB_CONFIG))
-Base.metadata.create_all(ENGINE_mysl)
-Session_mysql = sessionmaker(bind=ENGINE_mysl)
-session = Session_mysql()
