db724e5aec3488a47c8710bb91dbc5cc44a1f952
Freddy <buscando_alink@hotmail.com>
2018-06-17 01:10:01 -0400

    Undo changes, commit after every single inserted row

--- app/recollect.py
+++ app/recollect.py
@@ -3,10 +3,11 @@ this file is a helper to retrieve massive entries for
 Reddit from pushshift api
 '''
 
+from datetime import datetime
 from time import sleep as time_sleep
 from json import loads as json_loads
 from requests import get
-from app.db.persistence import save_posts
+from app.db.persistence import save_post
 
 HEADERS = {
     'User-Agent': 'osx:r/relationships.multiple.results:v1.0 (by /u/freddie'
@@ -16,20 +17,21 @@ URL = ('https://api.pushshift.io/reddit/search/submission/'
        '?filter=permalink,created_utc,title&size=1000&subreddit=AskReddit')
 
 
+def save_posts(posts):
+    for post in posts:
+        created_utc = datetime.utcfromtimestamp(post['created_utc'])
+        save_post(post['permalink'], created_utc)
+
+
 def perform_request(url):
     text_data = get(url, headers=HEADERS).text
     json = json_loads(text_data)
     return json['data']
 
 
-def pull_data(i = 10, after=None):
+def pull_save_data(after=None):
 
     time_sleep(2)
-
-    if i == 0: return
-    i -= 1
-
-    print(f' i => {i}')
 
     url = f'{URL}&before={after}' if after else URL
 
@@ -38,4 +40,4 @@ def pull_data(i = 10, after=None):
     save_posts(data)
 
     ref_name = data[-1]['created_utc']
-    pull_data(i, ref_name)
+    pull_save_data(ref_name)
